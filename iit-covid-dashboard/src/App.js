af77eb390c6dabc8e59e19f3188ad4559311225d
Justin Schmitz <jschmitz@uprightlaw.com>
2020-08-17 20:06:48 -0500

    learning react

--- iit-covid-dashboard/src/App.js
+++ iit-covid-dashboard/src/App.js
@@ -3,37 +3,126 @@ import logo from './logo.svg';
 import './App.css';
 
 
+class Food {
+  constructor(name, type, key) {
+    this.name = name;
+    this.type = type;
+    this.key = key;
+  }
+
+  getName() {
+    return (this.name + " " + this.name);
+  }
+
+  getType() {
+    return this.type;
+  }
+
+  getKey() {
+    return this.key;
+  }
+
+}
+
+const list = [
+  new Food("hot dog", "food", 0),
+  new Food("burrito", "food", 1)
+];
+
 class App extends Component {
-  render () {
-    const list = [
-      {
-      "name": "hot dog",
-      "type": "food",
-      "nutrition": "poor",
-      "objectID": 0
-      },
-      {
-        "name": "salad",
-        "type": "not food",
-        "nutrition": "poor",
-        "objectID": 1
-      }
-    ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+      searchTerm: ''
+      // Every time you modify this.state of a component,
+      // the component will redraw.
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(
+      item => item.getKey() != id);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  isSearched(searchTerm) {
+    return function (item) {
+      return item.getName().toLowerCase().includes(searchTerm.toLowerCase());
+    }
+  }
+
+  render() {
+    const { searchTerm, list } = this.state;
+
     return (
       <div className="App">
-      {list.map(item =>
-        <div key={item.objectID}>
-          <span>
-            {item.name},
-          </span>
-          <span>
-            {item.type}
-          </span>
-        </div>
-      )}
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Search
+          value={searchTerm.toLocaleLowerCase()}
+          onChange={this.onSearchChange}
+        />
+        <Search
+          value={searchTerm.toUpperCase()}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list={list}
+          searchTerm={searchTerm}
+          isSearched={this.isSearched}
+          onDismiss={this.onDismiss}
+        />
       </div>
+    )
+  }
+}
+
+class Search extends Component {
+  render() {
+    console.log(this);
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange} />
+      </form>
     );
   }
 }
+
+class Table extends Component {
+  render() {
+    console.log(this);
+    const { list, searchTerm, isSearched, onDismiss } = this.props;
+    return (
+      list.filter(isSearched(searchTerm))
+        .map(item =>
+          <div key={item.getKey()}>
+            <span>{item.getName()}</span>
+            <span>
+              <button
+                onClick={
+                  () => onDismiss(item.getKey())
+                } type="button">
+                Button
+                </button>
+            </span>
+          </div>)
+    )
+  }
+}
+
+// Page 79 of text
 
 export default App;
