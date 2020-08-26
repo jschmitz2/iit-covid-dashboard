12728deab184931e882c9175df0c8e46609c66a3
Justin Schmitz <jschmitz2@hawk.iit.edu>
2020-08-26 13:35:30 -0500

    Updates on remote server

--- iit-covid-dashboard/backend/image_gen.py
+++ iit-covid-dashboard/backend/image_gen.py
@@ -1,15 +1,16 @@
 import PIL
 from PIL import Image, ImageDraw, ImageFont
 import os
- 
+
 def gen_number_image(value, x_res=800, y_res=460, font_size=100):
     other_files = os.listdir()
-    filename = f'.\images\{value}_{x_res}_{y_res}.png'
+
+    filename = f'{value}_{x_res}_{y_res}.png'
 
     if filename in other_files:
         return os.getcwd() + "\\" + output.name
 
-    
+
     img = Image.new('RGB', (x_res, y_res), color = (40, 40, 40))
 
     fnt = ImageFont.truetype("HelveticaNeue.ttf", font_size)
@@ -34,12 +35,12 @@ def gen_number_image(value, x_res=800, y_res=460, font_size=100):
     )
 
 
-    output = open(filename, "wb")
+    output = open(("./images/" + filename), "wb")
     img.save(output, format="png")
     output.close()
 
     # HACK
-    return os.getcwd() + "\\" + output.name
+    return os.getcwd() + "/images/" + filename
 
 if __name__ == "__main__":
     print(os.getcwd())
